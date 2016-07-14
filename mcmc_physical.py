aa88b91b3ac4b3294cfcb45f80558d7c80556374
Jacob Lustig-Yaeger <jlustigy@uw.edu>
2016-07-14 17:46:41 +0200

    Added ColorPy module for plotting purposes

--- mcmc_physical.py
+++ mcmc_physical.py
@@ -15,6 +15,8 @@ from matplotlib import gridspec
 from matplotlib import rc
 import pdb
 
+from colorpy import colormodels, ciexyz
+
 from reparameterize import transform_Y2X
 
 mpl.rc('font', family='Times New Roman')
@@ -30,29 +32,6 @@ DIR = "mcmc_output/"
 DEFAULT_BURN_INDEX = 0
 
 #---------------------------------------------------
-"""
-def transform_Y2X(Y_array, n_band, n_slice):
-
-    Y_array = np.maximum(Y_array, -10)
-    Y_array = np.minimum(Y_array, 10)
-
-    # 'albedo' part
-    Y_albd_kj_flat = Y_array[0:N_TYPE*n_band]
-    X_albd_kj_flat = np.exp( Y_albd_kj_flat )/( 1 + np.exp( Y_albd_kj_flat ) )
-
-    # 'area fraction' part
-    Y_area_lk = Y_array[N_TYPE*n_band:].reshape([n_slice, N_TYPE-1])
-    expY_area_lk = np.exp( Y_area_lk )
-    expYY_area_lk = 1./( 1 + expY_area_lk )
-    cumprodY_area_lk = np.cumprod( expYY_area_lk, axis=1 )
-    X_area_lk = expY_area_lk * cumprodY_area_lk
-    X_area_lk = np.c_[X_area_lk, 1. - np.sum( X_area_lk, axis=1 )]
-    X_area_lk_flat = X_area_lk.flatten()
-
-    X_array = np.r_[ X_albd_kj_flat, X_area_lk_flat ]
-    return X_array
-"""
-
 def decomposeX(x,n_band,n_slice,n_type):
     alb = x[0:n_band * n_type].reshape((n_type,n_band))
     area = x[n_band * n_type:].reshape((n_slice , n_type))
